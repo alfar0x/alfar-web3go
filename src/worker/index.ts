3da439038cf755b8d212f73092c3de0e836fba4a
John Doe <johndoe@example.com>
2024-01-22 17:04:24 +0200

    minor

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -107,6 +107,7 @@ class Worker {
 
     for (const gift of filtered) {
       await postGiftOpen({ client: this.client, id: gift.id });
+      logger.info(`${this.name} | opened gift: ${gift.name}`);
       await wait(5, 10);
     }
 
